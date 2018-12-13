5e29538d7bd727540fbb0e8a03fd26d585e0f380
Explorer <propokerstar90@gmail.com>
2018-12-13 11:36:37 +0200

    created new components

---
+++ src/components/Buy & sell/Trading.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+
+class Trading extends React.Component {
+    render(){
+        return(
+            <div>
+                <h1>Buy and sell</h1>
+            </div>
+        )
+    }
+}
+
+export default Trading;
---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+
+class Dashboard extends React.Component {
+    render(){
+        return(
+            <div>
+                <h1>Buy and sell</h1>
+            </div>
+        )
+    }
+}
+
+export default Dashboard;
