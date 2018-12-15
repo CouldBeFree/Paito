c1e834a34bbf8e12aa4764c5b5f7da4925de71ca
Explorer <propokerstar90@gmail.com>
2018-12-15 20:57:17 +0200

    added user to the db

--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const users = require('./routes/api/user');
 const profile = require('./routes/api/profile');
 
 const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 //DB config
 const db = require('./keys/keys').mongoURI;
