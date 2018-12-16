f0dd9d4db68caa9c275b081ba42b267237fad6a9
Explorer <propokerstar90@gmail.com>
2018-12-16 18:00:26 +0200

    Added login form

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import {Router, Route, Link} from 'react-router-dom';
+//Reactstrap
 import { Row, Col } from 'reactstrap';
+//Components
 import Home from './Home/Home';
 import Dashboard from './Dashboard/Dashboard';
 import Trading from './Trading/Exchange';
@@ -11,18 +13,24 @@ import Wallet from './My wallet/Wallet';
 import Exchange from './Trading/Exchange';
 import Login from './Auth/Login';
 import Register from './Auth/Register';
+//Redux
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
 
 import createBrowserHistory from 'history/createBrowserHistory';
 import './App.css';
+
+const store = createStore(() => [], {}, applyMiddleware());
 
 const customHistory = createBrowserHistory();
 
 class App extends Component {
     render() {
         return (
-            <Router history={customHistory}>
-                <div>
-                    {/*<div className="top-bar">
+            <Provider store={store}>
+                <Router history={customHistory}>
+                    <div>
+                        {/*<div className="top-bar">
                         <Row>
                             <Col xs="3">
                                 <div className="logo-holder">
@@ -36,52 +44,53 @@ class App extends Component {
                             </Col>
                         </Row>
                     </div>*/}
-                    <div className="main-wrapper">
-                        <Row>
-                            <Col xs="3">
-                                <ul className="nav-holder">
-                                    <li className="nav-item">
-                                        <Link className="nav-anchor" to="/">Home</Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link className="nav-anchor" to="/dashboard">Dashboard</Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link className="nav-anchor" to="/trading">Trading</Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link className="nav-anchor" to="/marketcap">MarketCap</Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link className="nav-anchor" to="/ico">Ico</Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link className="nav-anchor" to="/buyandsell">Buy & Sell Crypto</Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link className="nav-anchor" to="/wallet">My Wallet</Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link className="nav-anchor" to="/exchange">Currency Exchange</Link>
-                                    </li>
-                                </ul>
-                            </Col>
-                            <Col xs="9">
-                                <Route exact path="/" component={Home} />
-                                <Route exact path="/register" component={Register} />
-                                <Route exact path="/login" component={Login} />
-                                <Route path="/dashboard" component={Dashboard} />
-                                <Route path="/trading" component={Trading} />
-                                <Route path="/marketcap" component={MarketCap}/>
-                                <Route path="/ico" component={Ico}/>
-                                <Route path="/buyandsell" component={BuyAndSell}/>
-                                <Route path="/wallet" component={Wallet}/>
-                                <Route path="/exchange" component={Exchange}/>
-                            </Col>
-                        </Row>
+                        <div className="main-wrapper">
+                            <Row>
+                                <Col xs="3">
+                                    <ul className="nav-holder">
+                                        <li className="nav-item">
+                                            <Link className="nav-anchor" to="/">Home</Link>
+                                        </li>
+                                        <li className="nav-item">
+                                            <Link className="nav-anchor" to="/dashboard">Dashboard</Link>
+                                        </li>
+                                        <li className="nav-item">
+                                            <Link className="nav-anchor" to="/trading">Trading</Link>
+                                        </li>
+                                        <li className="nav-item">
+                                            <Link className="nav-anchor" to="/marketcap">MarketCap</Link>
+                                        </li>
+                                        <li className="nav-item">
+                                            <Link className="nav-anchor" to="/ico">Ico</Link>
+                                        </li>
+                                        <li className="nav-item">
+                                            <Link className="nav-anchor" to="/buyandsell">Buy & Sell Crypto</Link>
+                                        </li>
+                                        <li className="nav-item">
+                                            <Link className="nav-anchor" to="/wallet">My Wallet</Link>
+                                        </li>
+                                        <li className="nav-item">
+                                            <Link className="nav-anchor" to="/exchange">Currency Exchange</Link>
+                                        </li>
+                                    </ul>
+                                </Col>
+                                <Col xs="9">
+                                    <Route exact path="/" component={Home} />
+                                    <Route exact path="/register" component={Register} />
+                                    <Route exact path="/login" component={Login} />
+                                    <Route path="/dashboard" component={Dashboard} />
+                                    <Route path="/trading" component={Trading} />
+                                    <Route path="/marketcap" component={MarketCap}/>
+                                    <Route path="/ico" component={Ico}/>
+                                    <Route path="/buyandsell" component={BuyAndSell}/>
+                                    <Route path="/wallet" component={Wallet}/>
+                                    <Route path="/exchange" component={Exchange}/>
+                                </Col>
+                            </Row>
+                        </div>
                     </div>
-                </div>
-            </Router>
+                </Router>
+            </Provider>
         );
     }
 }
--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,11 +1,51 @@
 import React from 'react';
+import { Row, Col, Button, Form, FormGroup, Input, Alert } from 'reactstrap';
+import { Link } from 'react-router-dom';
 import '../App.css';
 
 class Login extends React.Component {
+    state = {
+        password: '',
+        email: '',
+        errors: []
+    };
+
+    handleChange = (e) => {
+        this.setState({ [e.target.name] : e.target.value })
+    };
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
     render(){
+        const { errors } = this.state;
+
         return(
-            <div>
-                <h1>Login</h1>
+            <div className="main-wrapper">
+                <Row>
+                    <Col xs="3">
+                        <Form onSubmit={this.handleSubmit}>
+                            <FormGroup>
+                                <Input type="email" name="email" placeholder="Email" onChange={this.handleChange}/>
+                            </FormGroup>
+                            <FormGroup>
+                                <Input type="password" name="password" placeholder="Password" onChange={this.handleChange}/>
+                            </FormGroup>
+                            <Button>Submit</Button>
+                        </Form>
+                        <Link to="/register">REGISTER</Link>
+                        {errors.length > 0 && (
+                            <Alert color="danger" className="text-center">
+                                <h3>Something wrong</h3>
+                                {this.dispplayErrors(errors)}
+                            </Alert>
+                        )}
+                    </Col>
+                    <Col xs="9">
+
+                    </Col>
+                </Row>
             </div>
         )
     }
