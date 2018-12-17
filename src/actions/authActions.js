a3edce17117f202e3d6af463c8743d33eb0e4a8a
Sasha Zoria <alex.zoria@n3wnormal.com>
2018-12-17 16:49:00 +0200

    login reducer/action

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,5 +1,7 @@
-import { GET_ERRORS } from './types'
+import { GET_ERRORS, SET_CURRENT_USER } from './types'
 import axios from 'axios';
+import setAuthToken from '../utils/setAuthToken';
+import jwt_decode from 'jwt-decode';
 
 // Register User
 export const registerUser = (userData, history) => dispatch => {
@@ -11,4 +13,33 @@ export const registerUser = (userData, history) => dispatch => {
                 payload: err.response.data
             })
         })
+};
+
+//Login - Get User Token
+export const loginUser = userData => dispatch => {
+    axios.post('http://localhost:5000/api/users/login', userData)
+        .then(res => {
+            // Save to local storage
+            const { token } = res.data;
+            localStorage.setItem('jwtToken', token);
+            // Set token to Auth header
+            setAuthToken(token);
+            //Decode token to get user data
+            const decoded = jwt_decode(token);
+            // Set current user
+            dispatch(setCurrentUser(decoded))
+        })
+        .catch(err =>
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            }))
+};
+
+// Set logged in user
+export const setCurrentUser = (decoded) => {
+    return {
+        type: SET_CURRENT_USER,
+        payload: decoded
+    }
 };
