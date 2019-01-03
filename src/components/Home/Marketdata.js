c60af457b28c084fd3407fababd271c235cf2ec1
Explorer <propokerstar90@gmail.com>
2019-01-03 22:41:19 +0200

    get data from api to marketdata component

--- src/components/Home/Marketdata.js
+++ src/components/Home/Marketdata.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import axios from 'axios';
+import preloader from '../../preloader.gif';
 
 class Marketdata extends React.Component{
     state = {
         loadedData: [],
-        loading: false
+        loading: true
     };
 
     componentDidMount(){
@@ -16,8 +17,9 @@ class Marketdata extends React.Component{
             .then(res => {
                 console.log(res.data.Data);
                 this.setState({
-                    loadedData: res.data.Data
-                })
+                    loadedData: res.data.Data,
+                    loading: false
+                });
             })
             .catch(err => {
                 console.log(err);
@@ -27,11 +29,31 @@ class Marketdata extends React.Component{
     render(){
         const { loadedData, loading } = this.state;
         return(
-            <div>
-                <h1>Marketdata</h1>
-                <ul>
-
+            <div className="row">
+                <ul className="data-list col-sm-5">
+                    <li>
+                        <div className="head d-flex justify-content-between">
+                            <span>Coins</span>
+                            <span>Prices</span>
+                            <span>Change %</span>
+                        </div>
+                    </li>
+                    {
+                        loading ? <img className="preloader" src={preloader} alt="preloader"/> :
+                            Object.values(loadedData).map(item => {
+                                return <li key={item.CoinInfo.Id}>
+                                    <div className="content d-flex justify-content-between">
+                                        <span>{item.CoinInfo.Name}</span>
+                                        <span>{item.DISPLAY.USD.PRICE}</span>
+                                        <span>{item.DISPLAY.USD.CHANGEPCTDAY}</span>
+                                    </div>
+                                </li>
+                            })
+                    }
                 </ul>
+                <div className="col-sm-7">
+                    <h1>Graph</h1>
+                </div>
             </div>
         )
     }
