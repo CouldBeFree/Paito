166a98afac8e7746d55c80c1acc249bafce5172e
Sasha Zoria <alex.zoria@n3wnormal.com>
2019-01-04 11:25:15 +0200

    Styled marketdata component

--- src/components/Home/Marketdata.js
+++ src/components/Home/Marketdata.js
@@ -15,7 +15,6 @@ class Marketdata extends React.Component{
     getData = () => {
         axios.get('https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=USD')
             .then(res => {
-                console.log(res.data.Data);
                 this.setState({
                     loadedData: res.data.Data,
                     loading: false
@@ -30,27 +29,31 @@ class Marketdata extends React.Component{
         const { loadedData, loading } = this.state;
         return(
             <div className="row">
-                <ul className="data-list col-sm-5">
-                    <li>
-                        <div className="head d-flex justify-content-between">
-                            <span>Coins</span>
-                            <span>Prices</span>
-                            <span>Change %</span>
-                        </div>
-                    </li>
-                    {
-                        loading ? <img className="preloader" src={preloader} alt="preloader"/> :
-                            Object.values(loadedData).map(item => {
-                                return <li key={item.CoinInfo.Id}>
-                                    <div className="content d-flex justify-content-between">
-                                        <span>{item.CoinInfo.Name}</span>
-                                        <span>{item.DISPLAY.USD.PRICE}</span>
-                                        <span>{item.DISPLAY.USD.CHANGEPCTDAY}</span>
-                                    </div>
-                                </li>
-                            })
-                    }
-                </ul>
+                <div className="data-wrapper col-sm-5">
+                    <ul className="data-list">
+                        <li>
+                            <div className="head d-flex justify-content-between">
+                                <span>Coins</span>
+                                <span>Prices</span>
+                                <span>Change %</span>
+                            </div>
+                        </li>
+                        {
+                            loading ? <img className="preloader" src={preloader} alt="preloader"/> :
+                                Object.values(loadedData).map(item => {
+                                    return <li key={item.CoinInfo.Id}>
+                                        <div className="content d-flex justify-content-between">
+                                            <span>{item.CoinInfo.Name}</span>
+                                            <span>{item.DISPLAY.USD.PRICE}</span>
+                                            <span className={item.DISPLAY.USD.CHANGEPCTDAY > 0 ? 'green' : 'red'}>
+                                               <i className="fas fa-chevron-up"></i> {item.DISPLAY.USD.CHANGEPCTDAY} %
+                                            </span>
+                                        </div>
+                                    </li>
+                                })
+                        }
+                    </ul>
+                </div>
                 <div className="col-sm-7">
                     <h1>Graph</h1>
                 </div>
