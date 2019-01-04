8ee4af5336c24b284f122b566c00d6e8887e3ebc
Sasha Zoria <alex.zoria@n3wnormal.com>
2019-01-04 12:07:17 +0200

    added chart

--- src/components/Home/Marketdata.js
+++ src/components/Home/Marketdata.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import axios from 'axios';
 import preloader from '../../preloader.gif';
+import { Line } from 'react-chartjs-2'
 
 class Marketdata extends React.Component{
     state = {
         loadedData: [],
-        loading: true
+        loading: true,
+        chartData: {
+            labels: ['ETH', 'BTC', 'EOS', 'XPR', 'LTC', 'BCH'],
+            datasets:[
+                {
+                    label:'Population',
+                    data:[
+                        153.14,
+                        3833.51,
+                        0.359947,
+                        161.54,
+                        2.69,
+                        0.114041
+                    ],
+                    backgroundColor:[
+                        'rgba(255, 99, 132, 0.6)',
+                        'rgba(54, 162, 235, 0.6)',
+                        'rgba(255, 206, 86, 0.6)',
+                        'rgba(75, 192, 192, 0.6)',
+                        'rgba(153, 102, 255, 0.6)',
+                        'rgba(255, 159, 64, 0.6)',
+                        'rgba(255, 99, 132, 0.6)'
+                    ]
+                }
+            ]
+        }
     };
 
     componentDidMount(){
@@ -56,6 +82,9 @@ class Marketdata extends React.Component{
                 </div>
                 <div className="col-sm-7">
                     <h1>Graph</h1>
+                    <Line
+                        data={this.state.chartData}
+                    />
                 </div>
             </div>
         )
